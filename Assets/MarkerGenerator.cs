6f88013ae45ebc9e1a4d458155de59dfe529484c
CharlesAverill <charlesaverill20@gmail.com>
2020-09-04 01:54:48 -0500

    Markers sent every second

---
+++ Assets/MarkerGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.LSL4Unity.Scripts;
+
+public class MarkerGenerator : MonoBehaviour
+{
+
+    private LSLMarkerStream marker;
+    private int markerIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      marker = FindObjectOfType<LSLMarkerStream>();
+      markerIndex = 0;
+      InvokeRepeating("sendMarker", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void sendMarker(){
+      markerIndex += 1;
+      marker.Write("Marker " + markerIndex);
+    }
+}
